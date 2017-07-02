d04335fdfd0c5c19861e4b9d2e4118a33c9f7714
yantsong <songusepython@gmail.com>
2017-07-02 08:51:33 +0800

    init reactProject

---
+++ app/index.jsx
@@ -0,0 +1,5 @@
+
+  ReactDOM.render(
+        <h1>12Hello, wor11111sadsadsas111ld!</h1>
+        , document.getElementById('example') )
+
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        index: [
+            './app/index.jsx',
+            'webpack-dev-server/client?http://localhost:8080/'
+        ]
+    },
+    output: {
+        path: './dist',
+        filename: '[name][hash].js'
+    },
+    module: {
+
+        loaders: [
+            //单独css
+            { test: /\.css$/, loader: ExtractTextWebpackPlugin.extract({ use: "style-loader", ues: "css-loader" }) },
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+            //提供html内嵌的imgurl支持
+            { test: /\.html$/, loader: 'html-withimg-loader' },
+            { test: /\.(png|gif|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=8192&name=/resource/[name].[ext]' }
+            // { test: /\.less/, loader: 'style-loader!css-loader!less-loader'}
+        ]
+    },
+    externals: {
+        'react': 'React'
+    },
+    plugins: [
+        //提取公共模块
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'commons',
+            filename: 'js/base.js'
+        }),
+        //单独打包css
+        new ExtractTextWebpackPlugin('css/[name].css'),
+        //html模板插件
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: 'index2.html',
+            inject: true,
+            hash: true,
+            chunks: ['commons', 'index']
+        })
+    ],
+    // devServer: {
+    //     port: 8080,
+    //     inline: true
+    // }
+
+}
